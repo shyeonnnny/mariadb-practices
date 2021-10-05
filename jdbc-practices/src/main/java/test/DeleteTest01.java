819b3d4c7268eeea6323e11ea6a285f4801d51e9
shyeonnnny <ktmdgus970807@gmail.com>
2021-10-05 17:22:47 +0900

    2021.10.05

--- jdbc-practices/src/main/java/test/DeleteTest01.java
+++ jdbc-practices/src/main/java/test/DeleteTest01.java
@@ -3,18 +3,35 @@ package test;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class ConnectionTest {
+public class DeleteTest01 {
+
 	public static void main(String[] args) {
+		Boolean result = delete(5L);
+		System.out.println(result ? "성공" : "실패");
+	}
+
+	private static Boolean delete(long no) {
+		boolean result = false;
 		Connection conn = null; // (주소, 아이디, 비번)
+		Statement stmt = null;
 		try {
 			// 1. JDBC Driver 로딩
 			Class.forName("org.mariadb.jdbc.Driver");  // 클래스 로딩
 			
 			// 2. 연결하기
-			String url = "jdbc:mysql://127.0.0.1:3306/webdb?charset=utf8"; // ?charset=utf8은 옵션
-			conn = DriverManager.getConnection(url, "webdb", "webdb"); // (주소, 아이디, 비번)
+			String url = "jdbc:mysql://127.0.0.1:3306/employees?charset=utf8"; // ?charset=utf8은 옵션
+			conn = DriverManager.getConnection(url, "hr", "hr"); // (주소, 아이디, 비번)
 			
+			// 3. Statement 생성
+			stmt = conn.createStatement();
+			
+			// 4. SQL 실행
+			String sql="delete from dept where no = " + no;
+			int count = stmt.executeUpdate(sql);
+			
+			result = count == 1;
 			System.out.println("연결성공");
 			
 		} catch (ClassNotFoundException e) {
@@ -22,7 +39,11 @@ public class ConnectionTest {
 		} catch (SQLException e) {
 			System.out.println("error : " + e);
 		} finally { // close하면 자동 commit 됨
+			// clean up
 			try {
+				if(stmt != null) {
+					stmt.close();
+				}
 				if(conn != null) {
 					conn.close();
 				}
@@ -30,7 +51,7 @@ public class ConnectionTest {
 				System.out.println("error : " + e);
 			}
 		}
+		return result;
 	}
 
 }
-
--- jdbc-practices/src/main/java/test/InsertTest01.java
+++ jdbc-practices/src/main/java/test/InsertTest01.java
@@ -3,18 +3,36 @@ package test;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class ConnectionTest {
+public class InsertTest01 {
+
 	public static void main(String[] args) {
+		insert("영업");
+		insert("개발");
+		insert("기획");
+	}
+
+	private static boolean insert(String name) {
+		boolean result = false;
 		Connection conn = null; // (주소, 아이디, 비번)
+		Statement stmt = null;
 		try {
 			// 1. JDBC Driver 로딩
 			Class.forName("org.mariadb.jdbc.Driver");  // 클래스 로딩
 			
 			// 2. 연결하기
-			String url = "jdbc:mysql://127.0.0.1:3306/webdb?charset=utf8"; // ?charset=utf8은 옵션
-			conn = DriverManager.getConnection(url, "webdb", "webdb"); // (주소, 아이디, 비번)
+			String url = "jdbc:mysql://127.0.0.1:3306/employees?charset=utf8"; // ?charset=utf8은 옵션
+			conn = DriverManager.getConnection(url, "hr", "hr"); // (주소, 아이디, 비번)
 			
+			// 3. Statement 생성
+			stmt = conn.createStatement();
+			
+			// 4. SQL 실행
+			String sql="insert into dept values(null, '" + name + "')";
+			int count = stmt.executeUpdate(sql);
+			
+			result = count == 1;
 			System.out.println("연결성공");
 			
 		} catch (ClassNotFoundException e) {
@@ -22,7 +40,11 @@ public class ConnectionTest {
 		} catch (SQLException e) {
 			System.out.println("error : " + e);
 		} finally { // close하면 자동 commit 됨
+			// clean up
 			try {
+				if(stmt != null) {
+					stmt.close();
+				}
 				if(conn != null) {
 					conn.close();
 				}
@@ -30,7 +52,7 @@ public class ConnectionTest {
 				System.out.println("error : " + e);
 			}
 		}
+		return result;
 	}
 
 }
-
